c06b8d8875189ccb105540994828249cfc1abba2
senixcode <codesenix@gmail.com>
2021-07-29 10:26:46 -0500

    feat: :sparkles: config dotenv

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
+import dotenv from 'dotenv'
 import helmet from 'helmet';
-
+if (process.env.NODE_ENV === 'development') {
+  dotenv.config();
+}
 const app = express()
 
 import authRoutes from './routes/auth.routes'
 import userRoutes from './routes/user.routes'
-
+app.set('port', process.env.PORT || 5000)
 app.use(cors())
 app.use(helmet())
 app.use(morgan('dev'))
